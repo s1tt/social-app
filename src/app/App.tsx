2c23f107144b9421a284addc5888c56d188c9fae
Alekseev Sergei <nec13@mail.ru>
2023-12-04 10:10:09 +0300

    feat: add route config

---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+import { useTheme } from 'app/providers/ThemeProvider';
+import { Link } from 'react-router-dom';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from './providers/router';
+import './styles/index.scss';
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
+      <Link to='/'>Main</Link>
+      <Link to='/about'>About</Link>
+
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
