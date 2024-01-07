6e94266fb81eafbcd516017c674e25a4db120add
Alekseev Sergei <nec13@mail.ru>
2024-01-07 09:02:22 +0300

    fix build plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -19,10 +19,6 @@ export function buildPlugins({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
     })
   ];
 
